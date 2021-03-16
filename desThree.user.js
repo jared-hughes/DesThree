0610d2be33fc382d52a292c7b956c10415ede894
Jared Hughes <jahughes241@gmail.com>
2021-03-15 22:14:24 -0700

    Increment minor version → 0.2.0 (auto-update test)

--- desThree.user.js
+++ desThree.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DesThree
 // @namespace    http://github.com/jared-hughes
-// @version      0.1
+// @version      0.2.0
 // @description  Desmos bindings for three.js
 // @author       Jared Hughes (fireflame241)
 // @match        https://www.desmos.com/calculator/*
